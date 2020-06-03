6ad4989c0df56dd285dc68d844d70029671bdca0
Robert Szynal <rjszynal@gmail.com>
2020-06-03 19:23:38 +0100

    Update bootstrap script

--- scripts/desktop_bootstrap_debian.sh
+++ scripts/desktop_bootstrap_debian.sh
@@ -7,9 +7,6 @@ if [ "$EUID" -ne 0 ]; then
 	exit
 fi
 
-STORAGE_DIR='/mnt/storage'
-NORDELLE_DIR='/mnt/nordelle'
-
 export DEBIAN_FRONTEND=noninteractive
 
 # Choose a user account to use for this installation
@@ -31,29 +28,43 @@ if [ -z "${TARGET_USER-}" ]; then
 	done
 fi
 
+SERVICE_LIST=( "i3lock@${TARGET_USER}" )
+STORAGE_DIR='/mnt/storage'
+NORDELLE_DIR='/mnt/nordelle'
+
+install_fonts() {
+	# Ubuntu
+	wget https://assets.ubuntu.com/v1/0cef8205-ubuntu-font-family-0.83.zip -qO /tmp/ubuntu-font.zip
+	mkdir -p /home/${TARGET_USER}/.local/share/fonts/Ubuntu
+	unzip /tmp/ubuntu-font.zip -d /home/${TARGET_USER}/.local/share/fonts/Ubuntu/
+	rm -f /tmp/ubuntu-font.zip
+	# NerdFont Ubuntu Mono
+	wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/UbuntuMono.zip -qO /tmp/UbuntuMono.zip
+	mkdir -p /home/${TARGET_USER}/.local/share/fonts/NerdFonts
+	unzip /tmp/UbuntuMono.zip -d /home/${TARGET_USER}/.local/share/fonts/NerdFonts/
+	rm -f /tmp/UbuntuMono.zip
+}
+
 install_zsh() {
 	apt-get install -y --no-install-recommends zsh
-	# Install zsh theme
-	git clone https://github.com/bhilburn/powerlevel9k.git "${HOME}/.oh-my-zsh/custom/themes/powerlevel9k"
 }
 
 install_oh_my_zsh() {
 	install_zsh
-	wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -qO oh-my-zsh_install.sh
-	RUNZSH=no sh oh-my-zsh_install.sh
-	rm -f oh-my-zsh_install.sh
+	wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -qO /tmp/oh-my-zsh_install.sh
+	RUNZSH=no sh /tmp/oh-my-zsh_install.sh
+	rm -f /tmp/oh-my-zsh_install.sh
+	# Install zsh theme
+	git clone https://github.com/bhilburn/powerlevel9k.git "/home/${TARGET_USER}/.oh-my-zsh/custom/themes/powerlevel9k"
 }
 
 install_autofs() {
 	apt-get install -y --no-install-recommends autofs
 	mkdir -p /etc/auto.master.d
-	echo '/mnt	/etc/auto.mnt' > /etc/auto.master.d/storage.autofs
-	cat > /etc/auto.mnt <<-EOF
-		storage	 -rw,soft,intr,rsize=8192,wsize=8192 storage.szynal.co.uk:${STORAGE_DIR}
-		torrent  -rw,soft,intr,rsize=8192,wsize=8192 storage.szynal.co.uk:/home/${TARGET_USER}/torrent
-		nordelle -rw,soft,intr,rsize=8192,wsize=8192 nordelle.szynal.co.uk:${NORDELLE_DIR}
-	EOF
-	service autofs start
+	read -rp "Enter mount password: " mount_pass
+	echo "user=administrator" > /root/.storage.creds
+	echo "pass=${mount_pass}" >> /root/.storage.creds
+	SERVICE_LIST+=( autofs )
 }
 
 install_chrome() {
@@ -71,8 +82,9 @@ install_docker() {
 	systemctl enable docker
 	systemctl start docker
 	usermod -aG docker "${TARGET_USER}"
-	echo 'kernel.unprivileged_userns_clone=1' > /etc/sysctl.d/00-local-userns.conf
-	service procps restart
+	# Run the Docker daemon as a non-root user
+	sysctl --system
+	modprobe overlay permit_mounts_in_userns=1
 }
 
 install_google_drive() {
@@ -92,7 +104,6 @@ install_google_drive() {
 	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AD5F235DF639B041
 	apt-get update -y
 	apt-get install -y --no-install-recommends google-drive-ocamlfuse
-	sed -i '/#user_allow_other/ s/^#//' /etc/fuse.conf
 
 	google-drive-ocamlfuse "/home/${TARGET_USER}/googledrive-home"
 	#google-drive-ocamlfuse -label work "/home/${TARGET_USER}/googledrive-work"
@@ -100,20 +111,20 @@ install_google_drive() {
 
 install_lmms() {
 	download_url=$(curl --silent "https://api.github.com/repos/LMMS/lmms/releases/latest" | grep -Po '"browser_download_url": "\K.*?(?=")' | grep linux)
-	wget "${download_url}" -o "${HOME}/Downloads/lmms.AppImage"
-	chown "${TARGET_USER}": "${HOME}/Downloads/lmms.AppImage"
-	cat > "${HOME}/lmms.sh" <<-EOF
+	wget "${download_url}" -o "/home/${TARGET_USER}/Downloads/lmms.AppImage"
+	chown "${TARGET_USER}": "/home/${TARGET_USER}/Downloads/lmms.AppImage"
+	cat > "/home/${TARGET_USER}/lmms.sh" <<-EOF
 		#!/bin/bash
 
 		for directory in 'samples' 'soundfonts' 'lmms/projects'; do
-			rsync -av --delete ${STORAGE_DIR}/music/\${directory} \${HOME}/lmms/
+			rsync -av --delete ${STORAGE_DIR}/music/\${directory} \/home/${TARGET_USER}/lmms/
 		done
 
-		QT_SCALE_FACTOR=1.2 "\${HOME}/Downloads/lmms.AppImage"
+		QT_SCALE_FACTOR=1.2 "\/home/${TARGET_USER}/Downloads/lmms.AppImage"
 
-		rsync -av --delete \${HOME}/lmms/projects ${STORAGE_DIR}/music/lmms/
+		rsync -av --delete \/home/${TARGET_USER}/lmms/projects ${STORAGE_DIR}/music/lmms/
 		for directory in 'samples' 'soundfonts'; do
-			rsync -av --delete \${HOME}/lmms/\${directory} ${STORAGE_DIR}/music/
+			rsync -av --delete \/home/${TARGET_USER}/lmms/\${directory} ${STORAGE_DIR}/music/
 		done
 	EOF
 }
@@ -132,6 +143,50 @@ install_vscodium() {
 	apt-get install -y --no-install-recommends codium
 }
 
+install_traefik() {
+	scp nordelle.szynal.co.uk:/mnt/nordelle/backup/services/traefik.service.notls /etc/systemd/system/traefik.service
+	SERVICE_LIST+=( traefik )
+}
+
+install_folding() {
+	curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | \
+	sudo apt-key add -
+	distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+	curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list | \
+	sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
+	apt-get update -y
+	apt-get install -y nvidia-container-runtime libcuda1
+	mkdir -p /etc/systemd/system/docker.service.d
+	cat > /etc/systemd/system/docker.service.d/override.conf <<-EOF
+		[Service]
+		ExecStart=
+		ExecStart=/usr/bin/dockerd --host=fd:// --add-runtime=nvidia=/usr/bin/nvidia-container-runtime
+	EOF
+	scp nordelle.szynal.co.uk:/mnt/nordelle/backup/services/folding.service.local /etc/systemd/system/folding.service
+	SERVICE_LIST+=( folding )
+}
+
+install_i3() {
+	apt-get install -y \
+		--no-install-recommends \
+		feh \
+		i3 \
+		i3lock \
+		maim \
+		xclip \
+		xorg
+}
+
+install_awesome() {
+	apt-get install -y \
+		--no-install-recommends \
+		awesome \
+		i3lock \
+		maim \
+		xclip \
+		xorg
+}
+
 # Base
 apt-get update -y
 apt-get install -y \
@@ -142,47 +197,51 @@ apt-get install -y \
 	gnupg2 \
 	jq \
 	neovim \
+	nvidia-driver \
 	rsync \
 	silversearcher-ag \
 	software-properties-common \
+	thunar \
+	thunar-archive-plugin \
 	unzip \
 	wget
 install_docker
 install_google_drive
 
 # Window manager
+install_awesome
 apt-get install -y \
 	--no-install-recommends \
-	feh \
-	i3 \
-	i3lock \
-	maim \
-	suckless-tools \
-	xclip \
-	xorg
+	gnome-keyring
 
 # Desktop applications
 apt-get install -y \
 	--no-install-recommends \
 	firefox-esr \
+	gnome-terminal \
 	keepassxc \
 	terminator
 install_vscodium
 install_spotify
-install_lmms
+# install_lmms
 
 # Set up dev repos
 for repo in dotfiles dockerfiles; do
-	if [[ ! -d "${HOME}/development/github.com/rjszynal/${repo}" ]]; then
-		git clone https://github.com/RJSzynal/${repo}.git "${HOME}/development/github.com/rjszynal/${repo}/"
+	if [[ ! -d "/home/${TARGET_USER}/development/github.com/rjszynal/${repo}" ]]; then
+		mkdir -p "/home/${TARGET_USER}/development/github.com/rjszynal/"
+		git clone https://github.com/RJSzynal/${repo}.git "/home/${TARGET_USER}/development/github.com/rjszynal/${repo}/"
 	fi
-	if ! git --git-dir="${HOME}/development/github.com/rjszynal/${repo}/.git" remote -v | grep bitbucket; then 
-		git --git-dir="${HOME}/development/github.com/rjszynal/${repo}/.git" remote set-url --add --push origin git@bitbucket.org:RJSzynal/${repo}.git
+	if ! git --git-dir="/home/${TARGET_USER}/development/github.com/rjszynal/${repo}/.git" remote -v | grep bitbucket; then 
+		git --git-dir="/home/${TARGET_USER}/development/github.com/rjszynal/${repo}/.git" remote set-url --add --push origin git@bitbucket.org:RJSzynal/${repo}.git
 	fi
 done
 
 # Enable services
-sudo systemctl enable "i3lock@${TARGET_USER}"
+systemctl daemon-reload
+for service in ${SERVICE_LIST}; do
+	sudo systemctl enable ${service}
+	sudo systemctl start ${service}
+done
 
 # Cleanup
 apt autoremove
