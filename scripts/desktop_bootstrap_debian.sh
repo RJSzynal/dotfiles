3cc0e3c178a9733dc0792fe28ad644273ac65c85
Robert Szynal <rjszynal@gmail.com>
2023-06-25 15:53:52 +0100

    Move functions to a shared library

--- scripts/desktop_bootstrap_debian.sh
+++ scripts/desktop_bootstrap_debian.sh
@@ -14,179 +14,24 @@ if [ -z "${TARGET_USER-}" ]; then
 	mapfile -t options < <(find /home/* -maxdepth 0 -printf "%f\\n" -type d)
 	# if there is only one option just use that user
 	if [ "${#options[@]}" -eq "1" ]; then
-		readonly TARGET_USER="${options[0]}"
+		TARGET_USER="${options[0]}"
 		echo "Using user account: ${TARGET_USER}"
-		return
+	else
+		# iterate through the user options and print them
+		PS3='Which user account should be used? '
+
+		select opt in "${options[@]}"; do
+			TARGET_USER=$opt
+			break
+		done
 	fi
-
-	# iterate through the user options and print them
-	PS3='command -v user account should be used? '
-
-	select opt in "${options[@]}"; do
-		readonly TARGET_USER=$opt
-		break
-	done
 fi
 
-SERVICE_LIST=( "i3lock@${TARGET_USER}" )
+SERVICE_LIST=()
 STORAGE_DIR='/mnt/storage'
 NORDELLE_DIR='/mnt/nordelle'
 
-install_fonts() {
-	# Ubuntu
-	wget https://assets.ubuntu.com/v1/0cef8205-ubuntu-font-family-0.83.zip -qO /tmp/ubuntu-font.zip
-	mkdir -p /home/${TARGET_USER}/.local/share/fonts/Ubuntu
-	unzip /tmp/ubuntu-font.zip -d /home/${TARGET_USER}/.local/share/fonts/Ubuntu/
-	rm -f /tmp/ubuntu-font.zip
-	# NerdFont Ubuntu Mono
-	wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/UbuntuMono.zip -qO /tmp/UbuntuMono.zip
-	mkdir -p /home/${TARGET_USER}/.local/share/fonts/NerdFonts
-	unzip /tmp/UbuntuMono.zip -d /home/${TARGET_USER}/.local/share/fonts/NerdFonts/
-	rm -f /tmp/UbuntuMono.zip
-}
-
-install_zsh() {
-	apt-get install -y --no-install-recommends zsh
-}
-
-install_oh_my_zsh() {
-	install_zsh
-	wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -qO /tmp/oh-my-zsh_install.sh
-	KEEP_ZSHRC=yes RUNZSH=no sh /tmp/oh-my-zsh_install.sh
-	rm -f /tmp/oh-my-zsh_install.sh
-	# Install zsh theme
-	git clone --depth=1 https://github.com/bhilburn/powerlevel9k.git "/home/${TARGET_USER}/.oh-my-zsh/custom/themes/powerlevel9k"
-	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "/home/${TARGET_USER}/.oh-my-zsh/custom/themes/powerlevel10k"
-}
-
-install_autofs() {
-	apt-get install -y --no-install-recommends autofs
-	mkdir -p /etc/auto.master.d
-	read -rp "Enter mount password: " mount_pass
-	echo "user=administrator" > /root/.storage.creds
-	echo "pass=${mount_pass}" >> /root/.storage.creds
-	SERVICE_LIST+=( autofs )
-}
-
-install_chrome() {
-	curl https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
-	echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' > /etc/apt/sources.list.d/google-chrome.list
-	apt-get update -y
-	apt-get install -y --no-install-recommends google-chrome-stable
-}
-
-install_docker() {
-	wget -qO - https://download.docker.com/linux/debian/gpg | apt-key add -
-	echo "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
-	apt-get update -y
-	apt-get install -y --no-install-recommends docker-ce
-	systemctl enable docker
-	systemctl start docker
-	usermod -aG docker "${TARGET_USER}"
-	# Run the Docker daemon as a non-root user
-	sysctl --system
-	modprobe overlay permit_mounts_in_userns=1
-}
-
-install_google_drive() {
-	mkdir "/home/${TARGET_USER}/googledrive-home"
-	#mkdir "/home/${TARGET_USER}/googledrive-work"
-	chown "${TARGET_USER}": "/home/${TARGET_USER}/googledrive-home"
-	#chown "${TARGET_USER}": "/home/${TARGET_USER}/googledrive-work"
-
-	apt-get install -y \
-		--no-install-recommends \
-		fuse \
-		dirmngr
-	cat > /etc/apt/sources.list.d/alessandro-strada-ubuntu-ppa-bionic.list <<-"EOF"
-		deb http://ppa.launchpad.net/alessandro-strada/ppa/ubuntu xenial main
-		deb-src http://ppa.launchpad.net/alessandro-strada/ppa/ubuntu xenial main
-	EOF
-	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AD5F235DF639B041
-	apt-get update -y
-	apt-get install -y --no-install-recommends google-drive-ocamlfuse
-
-	google-drive-ocamlfuse "/home/${TARGET_USER}/googledrive-home"
-	#google-drive-ocamlfuse -label work "/home/${TARGET_USER}/googledrive-work"
-}
-
-install_lmms() {
-	download_url=$(curl --silent "https://api.github.com/repos/LMMS/lmms/releases/latest" | grep -Po '"browser_download_url": "\K.*?(?=")' | grep linux)
-	wget "${download_url}" -o "/home/${TARGET_USER}/Downloads/lmms.AppImage"
-	chown "${TARGET_USER}": "/home/${TARGET_USER}/Downloads/lmms.AppImage"
-	cat > "/home/${TARGET_USER}/lmms.sh" <<-EOF
-		#!/bin/bash
-
-		for directory in 'samples' 'soundfonts' 'lmms/projects'; do
-			rsync -av --delete ${STORAGE_DIR}/music/\${directory} \/home/${TARGET_USER}/lmms/
-		done
-
-		QT_SCALE_FACTOR=1.2 "\/home/${TARGET_USER}/Downloads/lmms.AppImage"
-
-		rsync -av --delete \/home/${TARGET_USER}/lmms/projects ${STORAGE_DIR}/music/lmms/
-		for directory in 'samples' 'soundfonts'; do
-			rsync -av --delete \/home/${TARGET_USER}/lmms/\${directory} ${STORAGE_DIR}/music/
-		done
-	EOF
-}
-
-install_spotify() {
-	curl -sSL https://download.spotify.com/debian/pubkey.gpg | apt-key add -
-	echo 'deb http://repository.spotify.com stable non-free' > /etc/apt/sources.list.d/spotify.list
-	apt-get update -y
-	apt-get install -y --no-install-recommends spotify-client
-}
-
-install_vscodium() {
-	curl -sSL https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | apt-key add -
-	echo 'deb https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/repos/debs/ vscodium main' > /etc/apt/sources.list.d/vscodium.list
-	apt-get update -y
-	apt-get install -y --no-install-recommends codium
-}
-
-install_traefik() {
-	scp nordelle.szynal.co.uk:/mnt/nordelle/backup/services/traefik.service.notls /etc/systemd/system/traefik.service
-	SERVICE_LIST+=( traefik )
-}
-
-install_folding() {
-	curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | \
-	sudo apt-key add -
-	distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-	curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list | \
-	sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
-	apt-get update -y
-	apt-get install -y nvidia-container-runtime libcuda1
-	mkdir -p /etc/systemd/system/docker.service.d
-	cat > /etc/systemd/system/docker.service.d/override.conf <<-EOF
-		[Service]
-		ExecStart=
-		ExecStart=/usr/bin/dockerd --host=fd:// --add-runtime=nvidia=/usr/bin/nvidia-container-runtime
-	EOF
-	scp nordelle.szynal.co.uk:/mnt/nordelle/backup/services/folding.service.local /etc/systemd/system/folding.service
-	SERVICE_LIST+=( folding )
-}
-
-install_i3() {
-	apt-get install -y \
-		--no-install-recommends \
-		feh \
-		i3 \
-		i3lock \
-		maim \
-		xclip \
-		xorg
-}
-
-install_awesome() {
-	apt-get install -y \
-		--no-install-recommends \
-		awesome \
-		i3lock \
-		maim \
-		xclip \
-		xorg
-}
+source ./library_debian.sh
 
 # Base
 apt-get update -y
@@ -194,27 +39,41 @@ apt-get install -y \
 	--no-install-recommends \
 	apt-transport-https \
 	ca-certificates \
+	curl \
+	dnsutils \
 	firmware-realtek \
+	git \
 	gnupg2 \
 	jq \
+	make \
 	neovim \
 	nvidia-driver \
 	rsync \
 	silversearcher-ag \
+	ssh \
 	software-properties-common \
 	thunar \
 	thunar-archive-plugin \
+	tig \
 	unzip \
 	wget
-install_docker
-install_google_drive
-install_oh_my_zsh
+install_docker "${TARGET_USER}"
+install_google_drive "${TARGET_USER}"
+install_oh_my_zsh "${TARGET_USER}"
+install_fonts "${TARGET_USER}"
 
 # Window manager
-install_awesome
+install_awesome "${TARGET_USER}"
 apt-get install -y \
 	--no-install-recommends \
 	gnome-keyring
+
+# Set up locale
+sed -i -e 's|^# \(en_GB.UTF-8\)|\1|' -e 's|^\(en_US.UTF-8\)|# \1|' /etc/locale.gen
+locale-gen
+
+# Set Neovim as global editor
+update-alternatives --set editor /usr/bin/nvim
 
 # Desktop applications
 apt-get install -y \
@@ -225,22 +84,25 @@ apt-get install -y \
 	terminator
 install_vscodium
 install_spotify
-# install_lmms
+# install_lmms "${TARGET_USER}" "${STORAGE_DIR}"
 
 # Set up dev repos
+sudo -u ${TARGET_USER} git config --global pull.ff only
 for repo in dotfiles dockerfiles; do
 	if [[ ! -d "/home/${TARGET_USER}/development/src/github.com/rjszynal/${repo}" ]]; then
-		mkdir -p "/home/${TARGET_USER}/development/src/github.com/rjszynal/"
-		git clone git@github.com:RJSzynal/${repo}.git "/home/${TARGET_USER}/development/src/github.com/rjszynal/${repo}/"
+		sudo -u ${TARGET_USER} mkdir -p "/home/${TARGET_USER}/development/src/github.com/rjszynal/"
+		sudo -u ${TARGET_USER} git clone git@github.com:RJSzynal/${repo}.git "/home/${TARGET_USER}/development/src/github.com/rjszynal/${repo}/"
 	fi
-	if ! git --git-dir="/home/${TARGET_USER}/development/src/github.com/rjszynal/${repo}/.git" remote -v | grep bitbucket; then
-		git --git-dir="/home/${TARGET_USER}/development/src/github.com/rjszynal/${repo}/.git" remote set-url --add --push origin git@bitbucket.org:RJSzynal/${repo}.git
-		git --git-dir="/home/${TARGET_USER}/development/src/github.com/rjszynal/${repo}/.git" remote set-url --add --push origin git@github.com:RJSzynal/${repo}.git
+	if ! sudo -u ${TARGET_USER} git --git-dir="/home/${TARGET_USER}/development/src/github.com/rjszynal/${repo}/.git" remote -v | grep bitbucket; then
+		sudo -u ${TARGET_USER} git --git-dir="/home/${TARGET_USER}/development/src/github.com/rjszynal/${repo}/.git" remote set-url --add --push origin git@bitbucket.org:RJSzynal/${repo}.git
+		sudo -u ${TARGET_USER} git --git-dir="/home/${TARGET_USER}/development/src/github.com/rjszynal/${repo}/.git" remote set-url --add --push origin git@github.com:RJSzynal/${repo}.git
 	fi
 done
 
 # Enable services
-systemctl daemon-reload
+if [ ${#SERVICE_LIST[@]} -gt 0 }; then
+	systemctl daemon-reload
+fi
 for service in ${SERVICE_LIST}; do
 	sudo systemctl enable ${service}
 	sudo systemctl start ${service}
--- scripts/wsl_bootstrap_debian.sh
+++ scripts/wsl_bootstrap_debian.sh
@@ -31,159 +31,7 @@ SERVICE_LIST=()
 STORAGE_DIR='/mnt/storage'
 NORDELLE_DIR='/mnt/nordelle'
 
-install_fonts() {
-	# Ubuntu
-	wget https://assets.ubuntu.com/v1/0cef8205-ubuntu-font-family-0.83.zip -qO /tmp/ubuntu-font.zip
-	mkdir -p /home/${TARGET_USER}/.local/share/fonts/Ubuntu
-	unzip /tmp/ubuntu-font.zip -d /home/${TARGET_USER}/.local/share/fonts/Ubuntu/
-	rm -f /tmp/ubuntu-font.zip
-	# NerdFont Ubuntu Mono
-	wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/UbuntuMono.zip -qO /tmp/UbuntuMono.zip
-	mkdir -p /home/${TARGET_USER}/.local/share/fonts/NerdFonts
-	unzip /tmp/UbuntuMono.zip -d /home/${TARGET_USER}/.local/share/fonts/NerdFonts/
-	rm -f /tmp/UbuntuMono.zip
-}
-
-install_zsh() {
-	apt-get install -y --no-install-recommends zsh
-}
-
-install_oh_my_zsh() {
-	install_zsh
-	wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -qO /tmp/oh-my-zsh_install.sh
-	KEEP_ZSHRC=yes RUNZSH=no sudo -u ${TARGET_USER} sh /tmp/oh-my-zsh_install.sh
-	rm -f /tmp/oh-my-zsh_install.sh
-	# Install zsh theme
-	sudo -u ${TARGET_USER} git clone --depth=1 https://github.com/bhilburn/powerlevel9k.git "/home/${TARGET_USER}/.oh-my-zsh/custom/themes/powerlevel9k"
-	sudo -u ${TARGET_USER} git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "/home/${TARGET_USER}/.oh-my-zsh/custom/themes/powerlevel10k"
-}
-
-install_autofs() {
-	apt-get install -y --no-install-recommends autofs
-	mkdir -p /etc/auto.master.d
-	read -rp "Enter mount password: " mount_pass
-	echo "user=administrator" > /root/.storage.creds
-	echo "pass=${mount_pass}" >> /root/.storage.creds
-	SERVICE_LIST+=( autofs )
-}
-
-install_chrome() {
-	curl https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
-	echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' > /etc/apt/sources.list.d/google-chrome.list
-	apt-get update -y
-	apt-get install -y --no-install-recommends google-chrome-stable
-}
-
-install_docker() {
-	wget -qO - https://download.docker.com/linux/debian/gpg | apt-key add -
-	echo "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
-	update-alternatives --set iptables /usr/sbin/iptables-legacy
-	apt-get update -y
-	apt-get install -y --no-install-recommends docker-ce docker-compose-plugin
-	# systemctl enable docker
-	# systemctl start docker
-	usermod -aG docker "${TARGET_USER}"
-}
-
-install_google_drive() {
-	mkdir "/home/${TARGET_USER}/googledrive-home"
-	#mkdir "/home/${TARGET_USER}/googledrive-work"
-	chown "${TARGET_USER}": "/home/${TARGET_USER}/googledrive-home"
-	#chown "${TARGET_USER}": "/home/${TARGET_USER}/googledrive-work"
-
-	apt-get install -y \
-		--no-install-recommends \
-		fuse \
-		dirmngr
-	cat > /etc/apt/sources.list.d/alessandro-strada-ubuntu-ppa-bionic.list <<-"EOF"
-		deb http://ppa.launchpad.net/alessandro-strada/ppa/ubuntu xenial main
-		deb-src http://ppa.launchpad.net/alessandro-strada/ppa/ubuntu xenial main
-	EOF
-	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AD5F235DF639B041
-	apt-get update -y
-	apt-get install -y --no-install-recommends google-drive-ocamlfuse
-
-	google-drive-ocamlfuse "/home/${TARGET_USER}/googledrive-home"
-	#google-drive-ocamlfuse -label work "/home/${TARGET_USER}/googledrive-work"
-}
-
-install_lmms() {
-	download_url=$(curl --silent "https://api.github.com/repos/LMMS/lmms/releases/latest" | grep -Po '"browser_download_url": "\K.*?(?=")' | grep linux)
-	wget "${download_url}" -o "/home/${TARGET_USER}/Downloads/lmms.AppImage"
-	chown "${TARGET_USER}": "/home/${TARGET_USER}/Downloads/lmms.AppImage"
-	cat > "/home/${TARGET_USER}/lmms.sh" <<-EOF
-		#!/bin/bash
-
-		for directory in 'samples' 'soundfonts' 'lmms/projects'; do
-			rsync -av --delete ${STORAGE_DIR}/music/\${directory} \/home/${TARGET_USER}/lmms/
-		done
-
-		QT_SCALE_FACTOR=1.2 "\/home/${TARGET_USER}/Downloads/lmms.AppImage"
-
-		rsync -av --delete \/home/${TARGET_USER}/lmms/projects ${STORAGE_DIR}/music/lmms/
-		for directory in 'samples' 'soundfonts'; do
-			rsync -av --delete \/home/${TARGET_USER}/lmms/\${directory} ${STORAGE_DIR}/music/
-		done
-	EOF
-}
-
-install_spotify() {
-	curl -sSL https://download.spotify.com/debian/pubkey.gpg | apt-key add -
-	echo 'deb http://repository.spotify.com stable non-free' > /etc/apt/sources.list.d/spotify.list
-	apt-get update -y
-	apt-get install -y --no-install-recommends spotify-client
-}
-
-install_vscodium() {
-	curl -sSL https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | apt-key add -
-	echo 'deb https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/repos/debs/ vscodium main' > /etc/apt/sources.list.d/vscodium.list
-	apt-get update -y
-	apt-get install -y --no-install-recommends codium
-}
-
-install_traefik() {
-	scp nordelle.szynal.co.uk:/mnt/nordelle/backup/services/traefik.service.notls /etc/systemd/system/traefik.service
-	SERVICE_LIST+=( traefik )
-}
-
-install_folding() {
-	curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | \
-	sudo apt-key add -
-	distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-	curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list | \
-	sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
-	apt-get update -y
-	apt-get install -y nvidia-container-runtime libcuda1
-	mkdir -p /etc/systemd/system/docker.service.d
-	cat > /etc/systemd/system/docker.service.d/override.conf <<-EOF
-		[Service]
-		ExecStart=
-		ExecStart=/usr/bin/dockerd --host=fd:// --add-runtime=nvidia=/usr/bin/nvidia-container-runtime
-	EOF
-	scp nordelle.szynal.co.uk:/mnt/nordelle/backup/services/folding.service.local /etc/systemd/system/folding.service
-	SERVICE_LIST+=( folding )
-}
-
-install_i3() {
-	apt-get install -y \
-		--no-install-recommends \
-		feh \
-		i3 \
-		i3lock \
-		maim \
-		xclip \
-		xorg
-}
-
-install_awesome() {
-	apt-get install -y \
-		--no-install-recommends \
-		awesome \
-		i3lock \
-		maim \
-		xclip \
-		xorg
-}
+source ./library_debian.sh
 
 # Base
 apt-get update -y
@@ -205,8 +53,8 @@ apt-get install -y \
 	tig \
 	unzip \
 	wget
-install_docker
-install_oh_my_zsh
+install_docker "${TARGET_USER}"
+install_oh_my_zsh "${TARGET_USER}"
 
 # Set up locale
 sed -i -e 's|^# \(en_GB.UTF-8\)|\1|' -e 's|^\(en_US.UTF-8\)|# \1|' /etc/locale.gen
@@ -229,7 +77,7 @@ for repo in dotfiles dockerfiles; do
 done
 
 # Enable services
-if [ ${#SERVICE_LIST[@]} -eq 0}; then
+if [ ${#SERVICE_LIST[@]} -gt 0 }; then
 	systemctl daemon-reload
 fi
 for service in ${SERVICE_LIST}; do
