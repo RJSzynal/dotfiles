566d8b602e223bc9f0a393b14601eaf27a790dbb
Robert Szynal <rjszynal@gmail.com>
2025-01-03 11:38:26 +0000

    Update Spotify Public Key

--- scripts/library_debian.sh
+++ scripts/library_debian.sh
@@ -203,7 +203,7 @@ install_oh_my_zsh() {
 }
 
 install_spotify() {
-	curl -s https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | gpg --dearmor --yes -o /usr/share/keyrings/spotify.gpg
+	curl -s https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | gpg --dearmor --yes -o /usr/share/keyrings/spotify.gpg
 	echo 'deb [signed-by=/usr/share/keyrings/spotify.gpg arch=amd64] http://repository.spotify.com stable non-free' \
 		> /etc/apt/sources.list.d/spotify.list
 	apt-get update -y
