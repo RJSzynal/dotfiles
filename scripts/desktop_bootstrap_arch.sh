b89cf440dbfd0420aeabe76807607cbf8f72beae
Robert Szynal <rjszynal@gmail.com>
2025-01-03 11:39:12 +0000

    Add Arch Linux desktop bootstrap scripts

---
+++ scripts/desktop_bootstrap_arch.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+if [ "${EUID}" -ne 0 ]; then
+	echo "Please run as root."
+	exit
+fi
+
+# Choose a user account to use for this installation
+if [ -z "${TARGET_USER-}" ]; then
+	mapfile -t options < <(find /home/* -maxdepth 0 -printf "%f\\n" -type d)
+	# if there is only one option just use that user
+	if [ "${#options[@]}" -eq "1" ]; then
+		TARGET_USER="${options[0]}"
+		echo "Using user account: ${TARGET_USER}"
+	else
+		# iterate through the user options and print them
+		PS3='Which user account should be used? '
+
+		select opt in "${options[@]}"; do
+			TARGET_USER=$opt
+			break
+		done
+	fi
+fi
+
+SERVICE_LIST=()
+STORAGE_DIR='/mnt/storage'
+NORDELLE_DIR='/mnt/nordelle'
+
+source ./library_arch.sh
+
+# Base
+pacman --noconfirm -Syu \
+		base-devel \
+		ca-certificates \
+		curl \
+		git \
+		gnupg \
+		jq \
+		linux-headers \
+		make \
+		neovim \
+		rsync \
+		the_silver_searcher \
+		tig \
+		unzip \
+		wget
+aur_install autofs
+install_docker "${TARGET_USER}"
+install_google_drive "${TARGET_USER}"
+install_oh_my_zsh "${TARGET_USER}"
+# install_fonts "${TARGET_USER}" # These are already comitted in the .fonts dir
+
+# # Wayland
+# pacman --noconfirm -S \
+# 		wayland \
+# 		xorg-xwayland
+
+# # File Browser
+# pacman --noconfirm -S \
+# 		thunar \
+# 		thunar-archive-plugin
+
+# Audio
+pacman --noconfirm -S \
+		alsa-utils \
+		pulseaudio
+
+# Graphics drivers
+pacman --noconfirm -S \
+		mesa \
+		vulkan-radeon \
+		xf86-video-amdgpu
+
+# Window manager
+# install_awesome "${TARGET_USER}"
+# install_gnome "${TARGET_USER}"
+install_kde "${TARGET_USER}"
+
+# Set up locale
+sed -i -e 's|^# \(en_GB.UTF-8\)|\1|' /etc/locale.gen
+locale-gen
+
+# Set Neovim as global editor
+update-alternatives --set editor /usr/bin/nvim
+
+# Desktop applications
+pacman --noconfirm -S \
+		firefox \
+		gnome-terminal \
+		spotify-launcher \
+		terminator \
+		vlc
+install_keepassxc
+install_vscodium
+install_steam
+# install_lmms "${TARGET_USER}" "${STORAGE_DIR}"
+
+# Set Terminator as global terminal
+update-alternatives --set x-terminal-emulator /usr/bin/terminator
+
+# Set up dev repos
+sudo -u ${TARGET_USER} git config --global pull.ff only
+for repo in dotfiles dockerfiles; do
+	if [[ ! -d "/home/${TARGET_USER}/development/src/github.com/rjszynal/${repo}" ]]; then
+		sudo -u ${TARGET_USER} mkdir -p "/home/${TARGET_USER}/development/src/github.com/rjszynal/"
+		sudo -u ${TARGET_USER} git clone git@github.com:RJSzynal/${repo}.git "/home/${TARGET_USER}/development/src/github.com/rjszynal/${repo}/"
+	fi
+	if ! sudo -u ${TARGET_USER} git --git-dir="/home/${TARGET_USER}/development/src/github.com/rjszynal/${repo}/.git" remote -v | grep bitbucket; then
+		sudo -u ${TARGET_USER} git --git-dir="/home/${TARGET_USER}/development/src/github.com/rjszynal/${repo}/.git" remote set-url --add --push origin git@bitbucket.org:RJSzynal/${repo}.git
+		sudo -u ${TARGET_USER} git --git-dir="/home/${TARGET_USER}/development/src/github.com/rjszynal/${repo}/.git" remote set-url --add --push origin git@github.com:RJSzynal/${repo}.git
+	fi
+	(cd "/home/${TARGET_USER}/development/src/github.com/rjszynal/${repo}/" \
+	&& git submodule update --init --recursive)
+done
+
+# Install XBox One Controller driver and firmware
+aur_install xone-dlundqvist-dkms-git
+aur_install xone-dongle-firmware
+
+# Install Logitech MX Master driver
+pacman --noconfirm -S solaar
+
+# Install Corsair keyboard driver
+aur_install ckb-next
+
+# Enable services
+if [ ${#SERVICE_LIST[@]} -gt 0 ]; then
+	systemctl daemon-reload
+fi
+for service in ${SERVICE_LIST}; do
+	systemctl enable ${service}
+done
+
+# Cleanup
+pacman --noconfirm -Sucy
---
+++ scripts/library_arch.sh
@@ -0,0 +1,213 @@
+#!/bin/bash
+
+install_awesome() {
+	pacman --noconfirm -S \
+		awesome \
+		gnome-keyring \
+		i3lock \
+		maim \
+		xclip \
+		xorg
+	SERVICE_LIST+=( i3lock@${1} )
+}
+
+install_autofs() {
+	pacman --noconfirm -S autofs
+
+	mkdir -p /etc/auto.master.d
+	if [[ ! -f /root/.storage.creds ]]; then
+		read -rp "Enter mount password: " mount_pass
+		echo "user=administrator" > /root/.storage.creds
+		echo "pass=${mount_pass}" >> /root/.storage.creds
+	fi
+	SERVICE_LIST+=( autofs )
+}
+
+install_docker() {
+	# Enable buildkit
+	if [ -f /etc/docker/daemon.json ]; then
+		cat /etc/docker/daemon.json | jq '.features.buildkit = true' > /etc/docker/daemon.json.tmp && mv /etc/docker/daemon.json{.tmp,}
+	else
+		jq --null-input '.features.buildkit = true' > /etc/docker/daemon.json
+	fi
+	# [ -z "$(uname -a | grep -i 'wsl')" ] || update-alternatives --set iptables /usr/sbin/iptables-legacy
+
+	# Install Docker
+	pacman --noconfirm -S \
+			docker \
+			docker-buildx
+	usermod -aG docker "${1}"
+	SERVICE_LIST+=( docker.socket )
+
+	# Created "trusted" user-defined bridge network
+	docker network create trusted
+
+	# # Run the Docker daemon as a non-root user
+	# if [ -z "$(uname -a | grep -i 'wsl')" ]; then
+	# 	systemctl disable --now docker.service docker.socket
+	# 	rm /var/run/docker.sock || true
+	# 	apt-get install -y \
+	# 			dbus-user-session \
+	# 			docker-ce-rootless-extras \
+	# 			slirp4netns \
+	# 			uidmap \
+	# 		--no-install-recommends
+	# 	sudo -u ${1} dockerd-rootless-setuptool.sh install
+	# fi
+}
+
+install_folding() {
+	scp nordelle.szynal.co.uk:/mnt/nordelle/backup/services/folding.service.local /etc/systemd/system/folding.service
+	SERVICE_LIST+=( folding )
+}
+
+install_fonts() {
+	# Ubuntu
+	if [[ ! -d "/home/${1}/.local/share/fonts/Ubuntu" ]]; then
+		wget https://assets.ubuntu.com/v1/0cef8205-ubuntu-font-family-0.83.zip -qO /tmp/ubuntu-font.zip
+		mkdir -p "/home/${1}/.local/share/fonts/Ubuntu"
+		unzip /tmp/ubuntu-font.zip -d "/home/${1}/.local/share/fonts/Ubuntu/"
+		rm -f /tmp/ubuntu-font.zip
+	fi
+	# NerdFont Ubuntu Mono
+	if [[ ! -d "/home/${1}/.local/share/fonts/NerdFonts" ]]; then
+		wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/UbuntuMono.zip -qO /tmp/UbuntuMono.zip
+		mkdir -p "/home/${1}/.local/share/fonts/NerdFonts"
+		unzip /tmp/UbuntuMono.zip -d "/home/${1}/.local/share/fonts/NerdFonts/"
+		rm -f /tmp/UbuntuMono.zip
+	fi
+}
+
+install_gnome() {
+	pacman -Sgq gnome \
+	| grep -v \
+		epiphany \
+		gnome-contacts \
+		gnome-music \
+		gnome-remote-desktop \
+		gnome-weather \
+	| pacman --noconfirm -S -
+
+	pacman --noconfirm -S \
+		spectacle
+}
+
+install_google_drive() {
+	mkdir -p "/home/${1}/googledrive-home"
+	#mkdir -p "/home/${1}/googledrive-work"
+	chown "${1}": "/home/${1}/googledrive-home"
+	#chown "${1}": "/home/${1}/googledrive-work"
+
+	aur_install google-drive-ocamlfuse
+
+	#google-drive-ocamlfuse "/home/${1}/googledrive-home"
+	#google-drive-ocamlfuse -label work "/home/${1}/googledrive-work"
+}
+
+install_i3() {
+	pacman --noconfirm -S \
+		feh \
+		i3-wm \
+		i3lock \
+		maim \
+		xclip
+	SERVICE_LIST+=( i3lock@${1} )
+}
+
+install_kde() {
+	pacman -Sgq plasma \
+	| grep -v \
+		plasma-welcome \
+	| pacman --noconfirm -S -
+
+	pacman --noconfirm -S \
+		spectacle
+}
+
+install_keepassxc() {
+	temp_pkgs=()
+	for pkg in gcc linux-headers libsystemd libxkbcommon; do
+		if [ -z "$(pacman -Q | grep ${pkg})" ]; then
+			temp_pkgs+=( ${pkg} )
+	done
+	pacman --noconfirm -S \
+			keepassxc \
+			mono-tools \
+			${temp_pkgs}
+
+	TMP_DIR=$(mktemp -d)
+	wget https://keepass.info/extensions/v2/kpuinput/KPUInput-1.4.zip -qO ${TMP_DIR}/kpuinput.zip
+	unzip ${TMP_DIR}/kpuinput.zip -d "${TMP_DIR}"
+	chmod +x ${TMP_DIR}/KPUInputN.sh
+	(cd ${TMP_DIR}; ./KPUInputN.sh)
+	mv ${TMP_DIR}/KPUInput{.*,N.so} /usr/lib/keepassxc/
+	cp -f /usr/lib/{keepassxc/,}KPUInputN.so
+	rm -rf ${TMP_DIR}
+
+	groupadd uinputg
+	usermod -a -G uinputg ${TARGET_USER}
+	echo 'KERNEL=="uinput", GROUP="uinputg", MODE="0660", OPTIONS+="static_node=uinput"' > /etc/udev/rules.d/89-uinput-u.rules
+
+	pacman --noconfirm -R ${temp_pkgs}
+}
+
+install_lmms() {
+	pacman --noconfirm -R lmms
+
+	cat > "/home/${1}/lmms.sh" <<-EOF
+		#!/bin/bash
+
+		for directory in 'samples' 'soundfonts' 'lmms/projects'; do
+			rsync -av --delete ${2}/music/\${directory} \/home/${1}/lmms/
+		done
+
+		QT_SCALE_FACTOR=1.2 /usr/bin/lmms
+
+		rsync -av --delete \/home/${1}/lmms/projects ${2}/music/lmms/
+		for directory in 'samples' 'soundfonts'; do
+			rsync -av --delete \/home/${1}/lmms/\${directory} ${2}/music/
+		done
+	EOF
+}
+
+install_oh_my_zsh() {
+	install_zsh
+	if [[ ! -d "/home/${1}/.oh-my-zsh" ]]; then
+		sudo -u ${1} sh -c "KEEP_ZSHRC=yes RUNZSH=no $(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	fi
+	# Install zsh theme
+	if [[ ! -d "/home/${1}/.oh-my-zsh/custom/themes/powerlevel10k" ]]; then
+		sudo -u ${1} git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "/home/${1}/.oh-my-zsh/custom/themes/powerlevel10k"
+	fi
+}
+
+install_steam() {
+	sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+	pacman -Syu \
+		lib32-mesa \
+		lib32-vulkan-radeon \
+		steam
+}
+
+install_traefik() {
+	scp nordelle.szynal.co.uk:/mnt/nordelle/backup/services/traefik.service.notls /etc/systemd/system/traefik.service
+	SERVICE_LIST+=( traefik )
+}
+
+install_vscodium() {
+	aur_install vscodium
+}
+
+aur_install() {
+	(
+		cd /tmp
+		git clone https://aur.archlinux.org/${1}.git
+		cd ${1}
+		makepkg -si
+	)
+	rm -rf /tmp/${1}
+}
+
+install_zsh() {
+	pacman --noconfirm -S zsh
+}
