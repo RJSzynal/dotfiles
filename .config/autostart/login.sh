d67b375756d93c49371519255ff3d551b61e1c4c
Robert Szynal <rjszynal@gmail.com>
2025-05-16 12:54:18 +0100

    Re-organise startup scripts

--- .config/autostart/login.sh
+++ .config/autostart/login.sh
@@ -2,10 +2,19 @@
 
 ## KDE/Gnome autostart script
 
+## Set all the relevant environment vars, functions, aliases, etc
 for file in ~/.{aliases,functions,path,dockerfunc,extra,exports}; do
-    if [[ -r "$file" ]] && [[ -f "$file" ]]; then
+    if [[ -r "${file}" ]] && [[ -f "${file}" ]]; then
         # shellcheck source=/dev/null
-        source "$file"
+        source "${file}"
+    fi
+done
+unset file
+
+## Execute scripts
+for file in ~/.{startup_apps_cli,startup_apps_gui}; do
+    if [[ -r "${file}" ]] && [[ -f "${file}" ]]; then
+        "${file}"
     fi
 done
 unset file
