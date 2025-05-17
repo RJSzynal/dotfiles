3b1392c0b5078fe0c16cf805575e69d24d9487e7
Robert Szynal <rjszynal@gmail.com>
2025-05-17 13:58:59 +0100

    Add logging to login script

--- .config/autostart/login.sh
+++ .config/autostart/login.sh
@@ -2,9 +2,13 @@
 
 ## KDE/Gnome autostart script
 
+# Ensure the log dir exists
+mkdir -p "${HOME}/log"
+
 ## Set all the relevant environment vars, functions, aliases, etc
-for file in ~/.{aliases,functions,path,dockerfunc,extra,exports}; do
+for file in ~/.{aliases,functions,path,dockerfunc,exports,extra}; do
     if [[ -r "${file}" ]] && [[ -f "${file}" ]]; then
+	    printf "%s %s %s:%s\n" $(date -u +'%T') "${0}" 'INFO' "Sourcing ${file}" >> ${HOME}/log/$(date +%F)-userscript.log
         # shellcheck source=/dev/null
         source "${file}"
     fi
@@ -14,6 +18,7 @@ unset file
 ## Execute scripts
 for file in ~/.{startup_apps_cli,startup_apps_gui}; do
     if [[ -r "${file}" ]] && [[ -f "${file}" ]]; then
+	    printf "%s %s %s:%s\n" $(date -u +'%T') "${0}" 'INFO' "Executing ${file}" >> ${HOME}/log/$(date +%F)-userscript.log
         "${file}"
     fi
 done
